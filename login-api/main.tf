ccecdf4f42270e1a162892a4d7cb2e855365013a
Oguzhan Uzman <ouzmann@gmail.com>
2021-04-18 14:19:59 +0300

    fix openapi yaml path

--- login-api/main.tf
+++ login-api/main.tf
@@ -1,7 +1,7 @@
 resource "aws_apigatewayv2_api" "login-api" {
   name          = "spotify-chat-login-api"
   protocol_type = "HTTP"
-  body          = file("./api/openapi.yaml")
+  body          = file("${path.module}/api/openapi.yaml")
 
   tags = {
     project = "spotify-chat"
