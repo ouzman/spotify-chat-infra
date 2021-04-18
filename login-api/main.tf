06a532fd6c42fb7a6adec51fb5fee0651d98b53b
Oguzhan Uzman <ouzmann@gmail.com>
2021-04-18 15:12:44 +0300

    move integrations to openapi.yaml file

--- login-api/main.tf
+++ login-api/main.tf
@@ -1,19 +1,11 @@
 resource "aws_apigatewayv2_api" "login_api" {
   name          = "spotify-chat-login-api"
   protocol_type = "HTTP"
-  body          = file("${path.module}/api/openapi.yaml")
+  body          = templatefile("${path.module}/api/openapi.yaml", { integrationInvokeArn: var.auth_lambda_invoke_arn })
 
   tags = {
     project = "spotify-chat"
   }
-}
-
-resource "aws_apigatewayv2_integration" "auth_lambda_integration" {
-  api_id           = aws_apigatewayv2_api.login_api.id
-  integration_type = "AWS_PROXY"
-
-  integration_method    = "POST"
-  integration_uri       = var.auth_lambda_invoke_arn
 }
 
 resource "aws_lambda_permission" "auth_lambda_permission" {
@@ -25,18 +17,4 @@ resource "aws_lambda_permission" "auth_lambda_permission" {
   # The "/*/*" portion grants access from any method on any resource
   # within the API Gateway REST API.
   source_arn = "${aws_apigatewayv2_api.login_api.execution_arn}/*/*"
-}
-
-resource "aws_apigatewayv2_route" "login_route_integration_attachment" {
-  api_id    = aws_apigatewayv2_api.login_api.id
-  route_key = "GET /login"
-
-  target = "integrations/${aws_apigatewayv2_integration.auth_lambda_integration.id}"
-}
-
-resource "aws_apigatewayv2_route" "callback_route_integration_attachment" {
-  api_id    = aws_apigatewayv2_api.login_api.id
-  route_key = "GET /callback"
-
-  target = "integrations/${aws_apigatewayv2_integration.auth_lambda_integration.id}"
 }
