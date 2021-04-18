7fc56506035e6844b0f789444b686b67af7b53e5
Oguzhan Uzman <ouzmann@gmail.com>
2021-04-18 16:32:42 +0300

    missing changes added

--- auth-lambda/js/src/auth-lambda.js
+++ auth-lambda/js/src/auth-lambda.js
@@ -1,3 +1,19 @@
-exports.handler = async (event, context) => {
-    return 'Hello world'
+const { log } = require('./util');
+const { handler: apiGatewayRouteHandler } = require('./handlers/api-gateway-route');
+const { handler: lambdaInvokeHandler } = require('./handlers/lambda-invoke');
+
+function handlerFactory({ event }) {
+    if (!!event.routeKey) {
+        return apiGatewayRouteHandler({ event });
+    } else if (!!event.eventType) {
+        return invokeLambdaHandler({ event });
+    }
 }
+
+exports.handler = async (event, context) => {
+    log({ event, context });
+
+    const handler = handlerFactory({ event });
+
+    return handler({ event });
+};
