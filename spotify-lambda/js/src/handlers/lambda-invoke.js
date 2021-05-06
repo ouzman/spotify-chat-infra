f024571900ec72281a0f809aa979ee382e9cf5e2
Oguzhan Uzman <ouzmann@gmail.com>
2021-05-06 03:42:21 +0300

    add error cases

--- spotify-lambda/js/src/handlers/lambda-invoke.js
+++ spotify-lambda/js/src/handlers/lambda-invoke.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch');
+const Request = require('node-fetch').Request;
 var fetchAbsolute = require('fetch-absolute');
 
 const { log } = require('../util');
@@ -9,36 +10,72 @@ const eventHandlers = {
     'getUserInfo': async ({ event }) => {
         const { accessToken } = event.payload;
 
-        const response = await spotifyApi('/v1/me', {
+        const request = new Request('/v1/me', {
             method: 'GET',
             headers: {
                 authorization: `Bearer ${accessToken}`,
             }
         });
 
+        let response;
+        try {
+            response = await spotifyApi(request);    
+        } catch (error) {
+            return networkError({ event, error });
+        }
+
         const responseBody = await response.json();
 
         log({ responseBody });
 
+        if (response.ok() === false) {
+            return errorResponse({ event, request, response: responseBody })
+        }
+
+        const eventPayload = {
+            uri: responseBody['uri'],
+            displayName: responseBody['display_name'],
+            image: responseBody['images'] && responseBody['images'][0] && responseBody['images'][0]['url'] || null,
+        };
+
         return {
             status: 0,
-            payload: {
-                responseBody,
-            },
+            payload: eventPayload,
         }
     },  
 }
 
+async function errorResponse({ event, request, response }) {
+    const jsonResponse = await response.json()
+    return {
+        status: response.error.status,
+        errorMessage: response.error.message,
+        context: {
+            request,
+            response
+        }
+    }
+}
 
-function unknown({ event }) {
+function networkError({ event, error }) {
+    return {
+        status: 2,
+        errorMessage: 'Network error occurred',
+        context: {
+            error
+        }
+    }
+}
+
+function unknownError({ event }) {
     return {
         status: 1,
-        body: "Unknown event type"
+        errorMessage: 'Unknown event type'
     }
 }
 
 exports.handler = async ({ event }) => {
     const { eventType } = event;
 
-    return eventHandlers[eventType] || unknown;
+    return eventHandlers[eventType] || unknownError;
 }
