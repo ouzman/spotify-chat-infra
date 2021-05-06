d1e32280e96791e0cf0bdb0af0e87e76a33a2cbf
Oguzhan Uzman <ouzmann@gmail.com>
2021-05-06 04:31:39 +0300

    fix function name on auth-lambda

--- auth-lambda/js/src/data-source/spotify.js
+++ auth-lambda/js/src/data-source/spotify.js
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 
 const lambda = new AWS.Lambda();
 
-const FUNCTION_NAME = process.env.API_KEYS_DB_TABLE_NAME
+const FUNCTION_NAME = process.env.SPOTIFY_LAMBDA_FUNCTION_NAME;
 
 exports.getUserInfo = async ({ accessToken }) => {
     const payload = {
