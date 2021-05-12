842e15ba252dfd0f4f20ec37d5c367daf6b8355f
Oguzhan Uzman <ouzmann@gmail.com>
2021-05-12 03:36:43 +0300

    minor changes on chat-lambda

--- chat-lambda/js/src/chat-lambda.js
+++ chat-lambda/js/src/chat-lambda.js
@@ -3,5 +3,22 @@ const { log } = require('./util');
 exports.handler = async (event, context) => {
     log({ event, context });
 
-    return { status: 'OK' };
+    if (event.requestContext.routeKey === '$disconnect' || event.requestContext.routeKey === '$connect') {
+        return { 
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'OK',
+            })
+         };    
+    }
+
+    if (event.routeKey === '$default') {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'OK',
+                body: event.requestContext.body,
+            })
+        }
+    }
 };
