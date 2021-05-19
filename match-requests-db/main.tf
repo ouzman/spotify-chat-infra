9dd8f112fd6f8dab16a56dcbe4f3d139c81a3ece
Oguzhan Uzman <ouzmann@gmail.com>
2021-05-19 05:59:16 +0300

    remove range key

--- match-requests-db/main.tf
+++ match-requests-db/main.tf
@@ -4,15 +4,9 @@ resource "aws_dynamodb_table" "match_requests" {
   read_capacity  = 5
   write_capacity = 5
   hash_key       = "SongId"
-  range_key      = "RequestDate"
 
   attribute {
     name = "SongId"
-    type = "S"
-  }
-
-  attribute {
-    name = "RequestDate"
     type = "S"
   }
 }
